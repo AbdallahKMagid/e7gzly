aa45478d01fbf8517237d4b0a4bdefeee18cc3c7
Abdallah Khaled <abdallah.kmagid@gmail.com>
2026-02-09 19:50:53 +0200

    made doc_logo_and_text

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,10 +1,21 @@
+import 'package:e7gzly/features/onboarding/widgets/doc_logo_and_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("this is onboardingScreen")));
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+            child: Column(children: [DocLogoAndText()]),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/onboarding/widgets/doc_logo_and_text.dart
@@ -0,0 +1,20 @@
+import 'package:e7gzly/core/theme/text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DocLogoAndText extends StatelessWidget {
+  const DocLogoAndText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset("assets/svgs/docdoc_logo.svg"),
+        SizedBox(width: 10.sp),
+        Text("E7gzly", style: TextStyles.font24Black700Weight),
+      ],
+    );
+  }
+}
