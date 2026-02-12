c2a3e105c64e972c6527af89bb4803655b30875f
Abdallah Khaled <abdallah.kmagid@gmail.com>
2026-02-12 19:47:59 +0200

    added appbar to login_screen

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,10 +1,24 @@
+import 'package:e7gzly/core/theme/color_manger.dart';
+import 'package:e7gzly/core/theme/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("this is loginScreen")));
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(color: Colors.white),
+        title: Text(
+          "Login Screen ",
+          style: TextStyles.font16WhiteWeight500.copyWith(fontSize: 18.sp),
+        ),
+        centerTitle: true,
+        backgroundColor: ColorManger.mainBlue,
+      ),
+      body: Center(child: Text("this is loginScreen")),
+    );
   }
 }
